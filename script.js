1c6f2dcc03a2f486829f0d9f5b1a9279d531ca3a
iamehho <ehhoehho369@gmail.com>
2026-01-28 22:49:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+function copyCA() {
+  const ca = document.getElementById("ca-text").innerText;
+  navigator.clipboard.writeText(ca);
+  alert("CA copied!");
+}
+
+function toggleCommunity() {
+  document.getElementById("community").classList.toggle("hidden");
+}
